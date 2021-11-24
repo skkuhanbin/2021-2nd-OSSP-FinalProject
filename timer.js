013d8ee5be643341e1cd866d73611f52f62625be
khj <khj98@skku.edu>
2021-11-25 05:45:39 +0900

    correct timer time, put timer count

--- timer.js
+++ timer.js
@@ -3,6 +3,7 @@ const timer = document.querySelector('.js-timer'),
   startBtn = document.querySelector('.js-timer__startBtn'),
   resetBtn = document.querySelector('.js-timer__resetBtn');
 
+let counterTIME = 3;
 let TIME = 0;
 let cron;
 
@@ -22,10 +23,17 @@ function resetButton() {
   timer.innerText = `00:00`;
   stopButton();
   timer.classList.remove('start');
+  counterTIME = 3;
   return (TIME = 0);
 }
 
 function updateTimer() {
+  if(counterTIME>0)
+  {
+    timer.innerText = `00:0${counterTIME} `;
+    counterTIME--;
+  }
+  else{
   const checkMinutes = Math.floor(TIME / 60);
   const seconds = TIME % 60;
   const minutes = checkMinutes % 60;
@@ -35,6 +43,7 @@ function updateTimer() {
   }:${seconds < 10 ? `0${seconds}` : seconds} `;
   TIME++;
   console.log(TIME - 1);
+}
 }
 
 function init() {
